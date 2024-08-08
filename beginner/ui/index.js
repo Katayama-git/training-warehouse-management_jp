8e3bc76704200913b9ae9e67ac053c5185d150d2
git-Ktayama <cgff0189@mail3.doshisha.ac.jp>
2024-08-08 12:41:21 +0900

    8月8日

--- beginner/ui/index.js
+++ beginner/ui/index.js
@@ -17,5 +17,30 @@ async function helloFromJava() {
 
 // TODO: helloExercise()を完成させて、サーバーからのメッセージをアラートで表示する
 async function helloExercise() {
-    alert("I'm not getting a message!");
+    const response = await fetch("http://localhost:8080/exercise");
+    if(!response.ok){
+        alert("Something wrong! Have you run the server yet?");
+        return;
+    }
+    console.log(response,"response");
+    const text = await response.text();
+    alert(text);
 }
+
+const box = 1;                 //constで定数boxを宣言し、1という数値の値を代入する。
+
+console.log(box,"box");        //コンソール上に定数の中身を表示する。
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+var variable;                          //variableという任意の名前の変数を宣言する。
+
+console.log(variable,"container1");    //変数に何も代入していない状態で変数の中身をコンソール上に表示する。
+
+variable = "apple";                    //変数variableにappleという文字列の値を代入する。
+
+variable = "アップル";                 //変数variableにアップルという文字列の値を再代入する。
+
+console.log(variable,'container2');    //コンソール上に変数variableの中身を表示する。
